462ce72241c459664a63c301b666ec8bbe10e2ee
Rahman Tabeshan <rtabeshan33@gmail.com>
2022-06-29 15:33:06 +0430

    set loader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
@@ -5,6 +6,26 @@ import LoginPage from "./Pages/LoginPage";
 import NotFound from "./Pages/Not-Found";
 
 const App = () => {
+
+    const [loading , setLoading] = useState(true) ;
+
+    useEffect(() => {
+        const onPageLoad = () => {
+          setLoading(false);
+        };
+    
+        // Check if the page has already loaded
+        if (document.readyState === "complete") {
+          onPageLoad();
+        } else {
+          window.addEventListener("load", onPageLoad);
+          // Remove the event listener when component unmounts
+          return () => window.removeEventListener("load", onPageLoad);
+        }
+      }, []);
+
+      if(loading) return <div>loading ... </div>
+
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,28 +1,11 @@
 
-import { useEffect, useState } from "react";
 import Home from "../Components/Home/Home";
 import Layout from "../Components/Layout/Layout";
 
 const HomePage = () => {
-    const [loading , setLoading] = useState(true) ;
-
-    useEffect(() => {
-        const onPageLoad = () => {
-          setLoading(false);
-        };
-    
-        // Check if the page has already loaded
-        if (document.readyState === "complete") {
-          onPageLoad();
-        } else {
-          window.addEventListener("load", onPageLoad);
-          // Remove the event listener when component unmounts
-          return () => window.removeEventListener("load", onPageLoad);
-        }
-      }, []);
 
     document.title = "فروشگاه اینترنتی دیجیکالا" ;
-    if(loading) return <div>loading...</div>
+
     return (
         <Layout>
             <Home />

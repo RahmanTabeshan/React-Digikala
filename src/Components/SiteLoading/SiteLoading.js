b99a202cb0df7214b693891668c6743e6e3135ed
Rahman Tabeshan <rtabeshan33@gmail.com>
2022-07-29 16:55:43 +0430

    fixed some bugs

--- src/Components/SiteLoading/SiteLoading.js
+++ src/Components/SiteLoading/SiteLoading.js
@@ -1,9 +1,9 @@
 const SiteLoading = () => {
     return (
         <div className="flex items-center justify-center gap-1 fixed inset-0 w-full h-screen z-50 ltr" > 
-            <span className="w-3 h-3 rounded-full animate-fade" ></span>
-            <span className="w-3 h-3 rounded-full animate-fade2" ></span>
-            <span className="w-3 h-3 rounded-full animate-fade3" ></span>
+            <span className="w-2 h-2 rounded-full animate-fade" ></span>
+            <span className="w-2 h-2 rounded-full animate-fade2" ></span>
+            <span className="w-2 h-2 rounded-full animate-fade3" ></span>
         </div>
     );
 }
